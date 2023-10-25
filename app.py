11e44a6c92041c0f8d80cd494f656b07db4b296f
BohdanTarchanin <32395057+BohdanTarchanin@users.noreply.github.com>
2023-10-25 11:58:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,33 +12,30 @@ import base64
 
 with st.sidebar:
         """
-         With the help of this web application, you will be able to get a forecast for your retail business. The program is designed for small and medium-sized businesses that already have a certain set of data about their sales and want to use it for analysis and forecasting.
-          To do this, you need to take the following steps:
-          1. Import the dataset
-          2. Enter the forecast interval
-          3. Familiarize yourself with data visualization
-          4. Download the forecast
+         With the help of this web application, you can forecast key healthcare metrics such as patient admissions, drug sales, or disease outbreak trends. Designed for healthcare professionals, institutions, or researchers with data sets ready for analysis and forecasting, the steps include:
+          1. Importing the dataset
+          2. Specifying the forecast interval
+          3. Analyzing data visualizations
+          4. Downloading the forecast
 
         """         
         
-st.image('Sales-forecast.jpg')
+# st.image('healthcare_forecast.jpg') 
 
-st.title('Automated forecasting for retail')
+st.title('Automated Forecasting for Healthcare Metrics')
 
 """
- This program will help you independently make the forecast you need
-specifically for your business. It can be a forecast of income, expenses, profit or the amount of goods sold. In order to make a high-quality forecast, it is necessary to collect
-a set of sales data in your business. And after that go through 4 steps in this web application to get a forecast based on your data.
+This application aids in crafting precise forecasts tailored to healthcare. Whether you're forecasting patient admissions, medical equipment demand, or disease trends, accurate data is the foundation. Start by gathering a coherent set of data for your healthcare institution or research. Afterward, follow the 4 steps in this web application to derive a forecast rooted in your data.
 """                     
 
 
 """
 ### Step 1: Import the dataset
 """
-df = st.file_uploader('Import your dataset here. A dataset is a table of sales data, in which the row is the date of sale and the column is various characteristics. The date column should be called ds and formatted in the following format: YYYY-MM-DD (Example: 2019-05-20). And in order for the program to understand what exactly you want to predict, mark this column y. The y column must be numeric and represent the measurement we want to predict. The file format is csv. Below will be a link to a design example.', type='csv')
+df = st.file_uploader('Upload your dataset here. The dataset should be structured with date of records and the desired metric. The date column must be labeled "ds" and follow this format: YYYY-MM-DD (Example: 2019-05-20). The metric column should be named "y", representing the numerical value you wish to forecast. Acceptable file format: csv. See the sample file structure below.', type='csv')
 st.info(
             f"""
-                👆 First load the file .csv. [File sample](https://raw.githubusercontent.com/facebook/prophet/main/examples/example_air_passengers.csv)
+                👆 First, upload the .csv file. [File sample](https://raw.githubusercontent.com/facebook/prophet/main/examples/example_air_passengers.csv)
                 """
         )
 
@@ -50,14 +47,14 @@ if df is not None:
     
     max_date = data['ds'].max()
     st.write(max_date)
-    st.success('👆 This is the end date in your data set')
+    st.success('👆 This marks the latest date in your dataset')
 
 """
-### Step 2: Enter the number of days for the forecast
+### Step 2: Specify the duration for the forecast
 
 """
 
-periods_input = st.number_input('For how many days do you want to make a forecast? Enter a number from 1 to 365 and click Enter',
+periods_input = st.number_input('How many days would you like the forecast to cover? Input a number between 1 and 365 then press Enter.',
 min_value = 1, max_value = 365)
 
 if df is not None:
@@ -66,13 +63,13 @@ if df is not None:
    
             
 """
-### Step 3: Visualization of forecast data
+### Step 3: Visualization of forecasted data
 
-The following table shows the result of the program - a forecast for the target column y, made on the basis of a historical data set.
-* ds - column with forecast dates
-* yhat is the predicted value of the target characteristic
-* yhat_lower - the probable lower limit of the predicted value
-* yhat_upper - the probable upper limit of the predicted value
+The table below presents the forecasting results for the target metric "y", based on your historical dataset.
+* ds - forecast dates
+* yhat - predicted value for the metric
+* yhat_lower - probable lower bound for the prediction
+* yhat_upper - probable upper bound for the prediction
 
 """
 if df is not None:
@@ -85,53 +82,45 @@ if df is not None:
     st.write(fcst_filtered)
     
     """
-    A bar chart displays the dependence of the date (ds) on the target characteristic (y) and the predicted interval.
-     The black dots are the actual figures from the historical data set, and the blue line is the forecast display.
+    The bar chart below illustrates the date (ds) against the target metric (y) and the prediction interval. The black dots represent the actual figures from the dataset, while the blue line is the forecast.
     """
     fig1 = m.plot(forecast)
     st.write(fig1)
     
-    st.info('The graph of the dependence of the date on the target value and the forecast')
-
+    st.info('Graph showcasing date dependency on the target metric alongside the forecast.')
 
     """
-    The following graphs show the trend for the target characteristic against the date and the seasonal correlation.
+    Up next, the graphs display the trend of the target metric against date and its seasonal correlation.
     """
     fig2 = m.plot_components(forecast)
     st.write(fig2)
       
-    st.info('Graphs of trend and periodic correlation')
+    st.info('Graphs visualizing the trend and seasonal patterns.')
 
     """    
-    With the help of interactive graphs, you can:
-     * visually increase the required period for a more detailed review
-     * choose an interval for viewing (week, month, year)
-     * download the graph as an image
+    Through interactive graphs, you can:
+     * Zoom into specific timeframes for detailed insights
+     * Opt for different viewing intervals (weekly, monthly, yearly)
+     * Download the visual as an image
     """
         
     fig3 = plot_plotly(m, forecast)
     st.write(fig3)
     
-    st.info('Interactive graph of the dependence of the date on the target value and the forecast')
-
+    st.info('Interactive chart detailing the date influence on the target metric and its forecast.')
 
     fig4 = plot_components_plotly(m, forecast)
     st.write(fig4)
       
-    st.info('Interactive graphs of trend and periodic correlation')              
+    st.info('Interactive charts elucidating trend and seasonal patterns.')              
 
 
 """
-### Step 4: Download the forecast for your business
+### Step 4: Download your healthcare metric forecast
 
-Click the "Download forecast" button and save the result of the program to your computer.
+To retain a copy of the forecast, click the "Download forecast" button and save the results to your device.
 """
 if df is not None:
     csv_exp = fcst_filtered.to_csv(index=False)
-    # When no file name is given, pandas returns the CSV as a string, nice.
-    b64 = base64.b64encode(csv_exp.encode()).decode()  # some strings <-> bytes conversions necessary here
-    #href = f'<a href="data:file/csv;base64,{b64}">Download CSV File</a> (right-click and save as ** &lt;forecast_name&gt;.csv**)'
-    #st.markdown(href, unsafe_allow_html=True)
-    st.download_button(label="Download forecast", data=csv_exp, file_name='prediction_data.csv', mime='text/csv')
-
-
+    b64 = base64.b64encode(csv_exp.encode()).decode()  
+    st.download_button(label="Download forecast", data=csv_exp, file_name='healthcare_metric_forecast.csv', mime='text/csv')
